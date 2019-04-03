21a559c83a598edc28e7758874c8d2c8f0c21ea3
ahadsenja <hamdanilm96@gmail.com>
2019-04-04 00:41:25 +0800

    feat: import and create barchart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import BarChart from './component/BarChart/BarChart'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <BarChart />
       </div>
     );
   }
